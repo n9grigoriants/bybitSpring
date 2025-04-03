9781e9b7d2d1306c5bf06e69a26e2e2e7ab89e4a
Ivan <pavivand@gmail.com>
2025-04-04 00:59:14 +0300

    213

--- src/main/java/ru/varino/bybit/services/UserService.java
+++ src/main/java/ru/varino/bybit/services/UserService.java
@@ -1,15 +1,18 @@
 package ru.varino.bybit.services;
 
 import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.varino.bybit.entities.UserEntity;
 import ru.varino.bybit.repositories.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    @Autowired
     private UserRepository userRepository;
+
+
 
     public void save(UserEntity user) {
         if (userRepository.existsByTgId(user.getTgId())) {
