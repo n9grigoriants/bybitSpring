1fe3be57d24bbaff93de0dddace37821db4bbd5d
Varinolav <pavivand@gmail.com>
2025-04-04 00:27:36 +0300

    minors

--- src/main/java/ru/varino/bybit/controllers/UserController.java
+++ src/main/java/ru/varino/bybit/controllers/UserController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
